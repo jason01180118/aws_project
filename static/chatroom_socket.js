3da07c7485630760f28d20328ff5825a506d9201
= <apple@Quan.local>
2022-12-09 20:54:25 +0800

    n

--- static/chatroom_socket.js
+++ static/chatroom_socket.js
@@ -5,23 +5,25 @@ var mic_mute_checkbox = document.querySelector("#mic_mute");
 var camera_enabled = camera_mute_checkbox.checked;
 var mic_enabled = mic_mute_checkbox.checked;
 var video = document.querySelector("#videoElement");
+
 document.addEventListener("DOMContentLoaded", (event)=>{
     // 當document被完整的讀取跟解析後就會被觸發
     startCamera();
-    camera_mute_checkbox.addEventListener('change', function() {
+});
+camera_mute_checkbox.addEventListener('change', function() {
         camera_enabled = camera_mute_checkbox.checked;
         setVideoMuteState(camera_enabled)
-    });
-    mic_mute_checkbox.addEventListener('change', function(){
-        mic_enabled = mic_mute_checkbox.checked;
-        setAudioMuteState(mic_enabled);
-    })
 });
-
+mic_mute_checkbox.addEventListener('change', function(){
+    mic_enabled = mic_mute_checkbox.checked;
+    setAudioMuteState(mic_enabled);
+})
 function startCamera(){
   navigator.mediaDevices.getUserMedia({ video: true, audio: true})
     .then(function (stream) {
       video.srcObject = stream;
+      setAudioMuteState(true);
+      setVideoMuteState(true);
     })
     .catch(function (err0r) {
       console.log("Something went wrong!");
@@ -34,6 +36,7 @@ function setVideoMuteState(flag){
     for(let i=0;i<track.length;i++) {
         track[i].enabled = flag;
     }
+    document.querySelector("#camera_mute").enabled = flag;
 }
 function setAudioMuteState(flag){
     let stream = video.srcObject;
@@ -41,4 +44,5 @@ function setAudioMuteState(flag){
     for(let i=0;i<track.length;i++) {
         track[i].enabled = flag;
     }
+    document.querySelector("#mic_mute").enabled = flag;
 }
