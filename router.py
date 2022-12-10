758f5fb9cfc67c4a54d192434e8f4aa1a2441f06
= <apple@Quan.local>
2022-12-10 14:24:10 +0800

    icon and checked

--- router.py
+++ router.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import send, emit
 
 
 def index():
     if request.method == 'POST':
         room = request.values.get('room')
+        # session['room_name'].append(room)
         if request.values.get('username') != '':
             return redirect(url_for("videoroom", room=room))
         return render_template("chatroom_added.html", room=room)
@@ -13,3 +14,7 @@ def index():
 
 def video_room(room):
     return render_template("video_room.html")
+
+
+def socket_io_test():
+    return render_template("chatroom.html")
