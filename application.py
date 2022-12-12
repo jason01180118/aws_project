d1584b7b14718ffb2b6860f853144e6ffd4b244b
Bryant Tang <96232475+Bryant-Tang@users.noreply.github.com>
2022-12-12 14:27:08 +0800

    fix: broadcast media state only to specific room

--- application.py
+++ application.py
@@ -13,9 +13,9 @@ application = Flask(__name__)
 application.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(application, cors_allowed_origins="*")
 
-_users_in_room = {} # stores room wise user list
-_room_of_sid = {} # stores room joined by an used
-_name_of_sid = {} # stores display name of users
+_users_in_room = {}  # stores room wise user list
+_room_of_sid = {}  # stores room joined by an used
+_name_of_sid = {}  # stores display name of users
 
 application.add_url_rule('/index', 'index',
                          index, methods=['GET', 'POST'])
@@ -35,31 +35,35 @@ def messageReceived(methods=['GET', 'POST']):
 def on_connect():
     sid = request.sid
     print("New socket connected ", sid)
-    
+
 
 @socketio.on("join-room")
 def on_join_room(data):
     sid = request.sid
     room_id = data["room_id"]
     display_name = session[room_id]["name"]
-    
+
     # register sid to the room
     join_room(room_id)
     _room_of_sid[sid] = room_id
     _name_of_sid[sid] = display_name
-    
+
     # broadcast to others in the room
     print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
-    emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
-    
+    emit("user-connect", {"sid": sid, "name": display_name},
+         broadcast=True, include_self=False, room=room_id)
+
     # add to user list maintained on server
     if room_id not in _users_in_room:
         _users_in_room[room_id] = [sid]
-        emit("user-list", {"my_id": sid}) # send own id only
+        emit("user-list", {"my_id": sid})  # send own id only
     else:
-        usrlist = {u_id:_name_of_sid[u_id] for u_id in _users_in_room[room_id]}
-        emit("user-list", {"list": usrlist, "my_id": sid}) # send list of existing users to the new member
-        _users_in_room[room_id].append(sid) # add new member to user list maintained on server
+        usrlist = {u_id: _name_of_sid[u_id]
+                   for u_id in _users_in_room[room_id]}
+        # send list of existing users to the new member
+        emit("user-list", {"list": usrlist, "my_id": sid})
+        # add new member to user list maintained on server
+        _users_in_room[room_id].append(sid)
 
     print("\nusers: ", _users_in_room, "\n")
 
@@ -71,7 +75,8 @@ def on_disconnect():
     display_name = _name_of_sid[sid]
 
     print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
-    emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
+    emit("user-disconnect", {"sid": sid},
+         broadcast=True, include_self=False, room=room_id)
 
     _users_in_room[room_id].remove(sid)
     if len(_users_in_room[room_id]) == 0:
@@ -91,13 +96,15 @@ def on_data(data):
         print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
 
     if data["type"] != "new-ice-candidate":
-        print('{} message from {} to {}'.format(data["type"], sender_sid, target_sid))
+        print('{} message from {} to {}'.format(
+            data["type"], sender_sid, target_sid))
     socketio.emit('data', data, room=target_sid)
 
 
 @socketio.on("state-change")
 def on_state_change(data):
-    socketio.emit('state-change',data)
+    socketio.emit('state-change', data, to=data["room"])
+
 
 if __name__ == '__main__':
     application.debug = True
